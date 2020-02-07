a08b6fc165a0687fe3cb309544bb17c4b2359d0f
ocean010101 <yangyang01.xu@outlook.com>
2020-02-07 10:32:31 +0800

    20 有效的括号
    Accepted
    76/76 cases passed (72 ms)
    Your runtime beats 41.64 % of javascript submissions
    Your memory usage beats 99.53 % of javascript submissions (33.4 MB)
    
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ 20.有效的括号.js
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (40.70%)
+ * Likes:    1364
+ * Dislikes: 0
+ * Total Accepted:    195.9K
+ * Total Submissions: 481.1K
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 
+ * 有效字符串需满足：
+ * 
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 注意空字符串可被认为是有效字符串。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "()"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "()[]{}"
+ * 输出: true
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "(]"
+ * 输出: false
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: "([)]"
+ * 输出: false
+ * 
+ * 
+ * 示例 5:
+ * 
+ * 输入: "{[]}"
+ * 输出: true
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let left = [], i, len = s.length;
+    for (i = 0; i < len; i++) {
+        let item = s[i];
+        if (item === '(' || item === '[' || item === '{') {
+            left.push(item);
+        } else if (item === ')') {
+            if (left[left.length - 1] !== '(') {
+                return false;
+            } else {
+                left.pop();
+            }
+        } else if (item === ']') {
+            if (left[left.length - 1] !== '[') {
+                return false;
+            } else {
+                left.pop();
+            }
+        } else if (item === '}') {
+            if (left[left.length - 1] !== '{') {
+                return false;
+            } else {
+                left.pop();
+            }
+        }
+    }
+    return left.length === 0;
+};
+// @lc code=end
+

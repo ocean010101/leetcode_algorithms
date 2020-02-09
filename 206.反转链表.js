c7cb60d14d686b5bbaa454db0b0de1e1437dfa08
ocean010101 <yangyang01.xu@outlook.com>
2020-02-09 11:08:51 +0800

    206 反转链表 Accepted 27/27 cases passed (100 ms) Your runtime beats 5.26 % of javascript submissions Your memory usage beats 68.91 % of javascript submissions (34.9 MB)
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ 206.反转链表.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (66.75%)
+ * Likes:    757
+ * Dislikes: 0
+ * Total Accepted:    151.9K
+ * Total Submissions: 227.4K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    let cur = head, prev = null;
+    while (cur) {
+        let nextTmp = cur.next;//保存当前节点对下一个节点的引用
+        cur.next = prev;// 当前节点的引用指向prev
+
+        prev = cur;//prev指向当前节点
+        cur = nextTmp;//跳到下一个节点
+    }
+    return prev;
+};
+// @lc code=end
+

f9a1f4484db1eeca16388d5e60a69570de8f155b
ocean010101 <yangyang01.xu@outlook.com>
2020-02-12 14:32:29 +0800

    231.2-的幂
    Accepted
    1108/1108 cases passed (96 ms)
    Your runtime beats 17.71 % of javascript submissions
    Your memory usage beats 63.83 % of javascript submissions (35.5 MB)
    
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ 231.2-的幂.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=231 lang=javascript
+ *
+ * [231] 2的幂
+ *
+ * https://leetcode-cn.com/problems/power-of-two/description/
+ *
+ * algorithms
+ * Easy (47.45%)
+ * Likes:    159
+ * Dislikes: 0
+ * Total Accepted:    44.7K
+ * Total Submissions: 94K
+ * Testcase Example:  '1'
+ *
+ * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1
+ * 输出: true
+ * 解释: 2^0 = 1
+ * 
+ * 示例 2:
+ * 
+ * 输入: 16
+ * 输出: true
+ * 解释: 2^4 = 16
+ * 
+ * 示例 3:
+ * 
+ * 输入: 218
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+    return Number.isInteger(Math.log2(n));
+};
+// @lc code=end
+

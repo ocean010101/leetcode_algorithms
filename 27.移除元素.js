853750a85681d9e43bd3c50c7cd2d43235cfca98
ocean010101 <yangyang01.xu@outlook.com>
2020-02-13 17:19:07 +0800

    27.移除元素
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ 27.移除元素.js
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=javascript
+ *
+ * [27] 移除元素
+ *
+ * https://leetcode-cn.com/problems/remove-element/description/
+ *
+ * algorithms
+ * Easy (57.30%)
+ * Likes:    480
+ * Dislikes: 0
+ * Total Accepted:    134K
+ * Total Submissions: 233.6K
+ * Testcase Example:  '[3,2,2,3]\n3'
+ *
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ * 
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * 
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * 
+ * 示例 1:
+ * 
+ * 给定 nums = [3,2,2,3], val = 3,
+ * 
+ * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+ * 
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+ * 
+ * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+ * 
+ * 注意这五个元素可为任意顺序。
+ * 
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 
+ * 
+ * 说明:
+ * 
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ * 
+ * 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ * 
+ * 你可以想象内部操作如下:
+ * 
+ * // nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+ * int len = removeElement(nums, val);
+ * 
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+/*
+Accepted
+113/113 cases passed (52 ms)
+Your runtime beats 99.17 % of javascript submissions
+Your memory usage beats 89.73 % of javascript submissions (33.6 MB)
+*/
+var removeElement1 = function (nums, val) {
+    //splice()
+    for (let i = 0; i < nums.length; i++) {
+        if (typeof (nums[i]) != 'undefined') {
+            if (nums[i] === val) {
+                nums.splice(i, 1);
+                i--;
+            }
+        } else {
+            break;
+        }
+    }
+    return nums.length;
+};
+
+/*
+Accepted
+113/113 cases passed (64 ms)
+Your runtime beats 76.53 % of javascript submissions
+Your memory usage beats 86.85 % of javascript submissions (33.6 MB)
+
+*/
+var removeElement2 = function (nums, val) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};
+
+/*
+Accepted
+113/113 cases passed (60 ms)
+Your runtime beats 90.21 % of javascript submissions
+Your memory usage beats 86.85 % of javascript submissions (33.6 MB)
+*/
+
+var removeElement = function (nums, val) {
+    let i = 0,
+        n = nums.length;
+    while (i < n) {
+        if (nums[i] == val) {
+            nums[i] = nums[n - 1];
+            // reduce array size by one
+            n--;
+        } else {
+            i++;
+        }
+    }
+    return n;
+};
+// @lc code=end
+

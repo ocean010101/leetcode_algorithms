039c37fc4fc85b8771984b267d4251e28e465e3a
ocean010101 <yangyang01.xu@outlook.com>
2020-02-16 12:15:11 +0800

    344.反转字符串--update
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- 344.反转字符串.js
+++ 344.反转字符串.js
@@ -39,11 +39,46 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function (s) {
+/*
+Accepted
+478/478 cases passed (136 ms)
+Your runtime beats 63.6 % of javascript submissions
+Your memory usage beats 42.76 % of javascript submissions (47.2 MB)
+*/
+var reverseString2 = function (s) {
     let len = s.length, len2 = len / 2;
     for (let i = 0; i < len2; i++) {
         [s[i], s[len - 1 - i]] = [s[len - 1 - i], s[i]];
     }
+};
+
+/*
+Accepted
+478/478 cases passed (120 ms)
+Your runtime beats 97.69 % of javascript submissions
+Your memory usage beats 88.54 % of javascript submissions (46.8 MB)
+*/
+var reverseString = function (s) {
+    return s.reverse();
+};
+/*
+Accepted
+478/478 cases passed (136 ms)
+Your runtime beats 63.6 % of javascript submissions
+Your memory usage beats 5.02 % of javascript submissions (49.1 MB)
+*/
+var helper = function (str, left, right) {
+    if (left >= right) {
+        return;
+    }
+    let tmp = str[left];
+    str[left++] = str[right];
+    str[right--] = tmp;
+    return helper(str, left, right);
+}
+
+var reverseString1 = function (s) {
+    helper(s, 0, s.length - 1);
 };
 // @lc code=end
 

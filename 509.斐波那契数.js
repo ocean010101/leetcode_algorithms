c3bfc3b5cf4a525eccc3efe5255144273bb1fd4f
ocean010101 <yangyang01.xu@outlook.com>
2020-02-17 12:04:04 +0800

    509.斐波那契数
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -9,14 +9,45 @@
  * @param {number} N
  * @return {number}
  */
+/*
+递归
+Accepted
+31/31 cases passed (92 ms)
+Your runtime beats 20.71 % of javascript submissions
+Your memory usage beats 16.43 % of javascript submissions (34.5 MB)
+*/
 var fib = function (N) {
-    // 1.递归
-    // if (N == 1 || N == 0) {
-    //     return N;
-    // }
-    // return fib(N - 1) + fib(N - 2);
-    //2. 递推 O(n)
-    let cache = [];
+    if (N < 2) {
+        return N;
+    }
+    return fib(N - 1) + fib(N - 2);
+}
+
+/*
+Accepted
+31/31 cases passed (68 ms)
+Your runtime beats 53.41 % of javascript submissions
+Your memory usage beats 58.13 % of javascript submissions (33.9 MB)
+*/
+let cache = {};
+var fib1 = function (N) {
+    if (cache[N]) {
+        return cache[N]
+    }
+
+      let result;
+      if (N < 2) {
+        result = N;
+      } else {
+        result = fib(N-1) + fib(N-2);
+      }
+      cache[N] = result;
+      return result;
+}
+
+//递推
+var fib2 = function (N) {
+    let cache = []; // 用来
     for (let i = 0; i <= N; i++) {
         if (i == 1 || i == 0) {
             cache[i] = i;

7264c2d9c4445b54e6794984a9116e3f9af295ea
ocean010101 <yangyang01.xu@outlook.com>
2020-02-23 12:28:25 +0800

    278.第一个错误的版本-二分查找模板2
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ 278.第一个错误的版本.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=javascript
+ *
+ * [278] 第一个错误的版本
+ *
+ * https://leetcode-cn.com/problems/first-bad-version/description/
+ *
+ * algorithms
+ * Easy (36.59%)
+ * Likes:    137
+ * Dislikes: 0
+ * Total Accepted:    36.2K
+ * Total Submissions: 98.7K
+ * Testcase Example:  '5\n4'
+ *
+ * 
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ * 
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ * 
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version
+ * 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ * 
+ * 示例:
+ * 
+ * 给定 n = 5，并且 version = 4 是第一个错误的版本。
+ * 
+ * 调用 isBadVersion(3) -> false
+ * 调用 isBadVersion(5) -> true
+ * 调用 isBadVersion(4) -> true
+ * 
+ * 所以，4 是第一个错误的版本。 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+/*
+Accepted
+22/22 cases passed (68 ms)
+Your runtime beats 35.22 % of javascript submissions
+Your memory usage beats 63.06 % of javascript submissions (33.8 MB)
+*/
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let left = 1,
+            right = n;
+        while (left < right) {
+            let mid = left + Math.floor((right - left) / 2);
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    };
+};
+// @lc code=end
+

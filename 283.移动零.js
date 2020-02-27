5e79c88ba56ff3743ca3e8d1651207da136894a6
ocean010101 <yangyang01.xu@outlook.com>
2020-02-27 20:00:00 +0800

    283.移动零
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- 283.移动零.js
+++ 283.移动零.js
@@ -40,13 +40,40 @@ Accepted
 Your runtime beats 61.87 % of javascript submissions
 Your memory usage beats 5.16 % of javascript submissions (36.9 MB)
 */
-var moveZeroes = function (nums) {
+var moveZeroes1 = function (nums) {
     let lastNonZeroFoundAt = 0;
     for (let cur = 0; cur < nums.length; cur++) {
         if (nums[cur] != 0) {
             [nums[lastNonZeroFoundAt], nums[cur]] = [nums[cur], nums[lastNonZeroFoundAt]];
             lastNonZeroFoundAt++;
         }
+    }
+};
+/*
+思路：
+把数组中所有的非零元素，按顺序给数组的前段元素位赋值，
+剩下的全部直接赋值 0。我们定义一个 nums 0...i 表示为非 0 元素的数组，之后在遍历数列的时候不断维护这个定义。
+时间复杂度：O(n)
+空间复杂度：O(1)
+Accepted
+21/21 cases passed (68 ms)
+Your runtime beats 91.68 % of javascript submissions
+Your memory usage beats 59.8 % of javascript submissions (35.8 MB)
+*/
+var moveZeroes = function (nums) {
+    let n = nums.length,
+        i = 0,
+        j = 0;
+
+    while (j <= n - 1) {
+        if (nums[j] != 0) {
+            nums[i] = nums[j];
+            i++;
+        }
+        j++;
+    }
+    for (let k = i; k < n; k++) {
+        nums[k] = 0;
     }
 };
 // @lc code=end
